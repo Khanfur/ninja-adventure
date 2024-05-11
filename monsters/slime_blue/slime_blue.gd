08c760c478266941b2109deb8dd49c4e7928f7c5
Brandon <brandon.tromp@gmail.com>
2024-05-11 14:20:44 +0200

    added slime movement

--- monsters/slime_blue/slime_blue.gd
+++ monsters/slime_blue/slime_blue.gd
@@ -1,11 +1,42 @@
-extends AnimatedSprite2D
+extends CharacterBody2D
 
+var speed = 20
+var limit = 0.5
+@export var endPoint: Marker2D
 
-# Called when the node enters the scene tree for the first time.
+var startPosition: Vector2
+var endPosition: Vector2
+
+@onready var animations = $AnimatedSprite2D
+
 func _ready():
-	pass # Replace with function body.
+	startPosition = position
+	
+	if !endPoint:
+		endPosition = startPosition + Vector2(0, 3*16)
+		return
+	
+	endPosition = endPoint.global_position
 
+func changeDirection():
+	var tempEnd = endPosition
+	endPosition = startPosition
+	startPosition = tempEnd
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func updateVelocity():
+	var moveDirection = endPosition - position
+	if moveDirection.length() < limit:
+		position = endPosition
+		changeDirection()
+	velocity = moveDirection.normalized() * 20
+
+func updateAnimation():
+	if velocity.y > 0:
+		animations.play("move_down")
+	else: 
+		animations.play("move_up")
+
+func _physics_process(delta):
+	updateVelocity()
+	move_and_slide()
+	updateAnimation()
