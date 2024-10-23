f0d201d5cc47915529197704941648c254a3154e
Brandon <brandon.tromp@coolgames.com>
2024-10-23 19:08:30 +0200

    added walking animation for the player

--- player/player.gd
+++ player/player.gd
@@ -2,10 +2,27 @@ extends CharacterBody2D
 
 @export var movementSpeed: int = 35
 
-func handleInput():
+@onready var _animation_player = $playerMovement
+
+func handleInput() -> void:
 	var movementDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = movementDirection * movementSpeed
+
+func updateAnimation() -> void:
 	
+	if velocity.length() == 0: _animation_player.stop()
+	else:
+		var inputDirection = "Down"
+		
+		if Input.is_action_pressed("ui_right"):
+			inputDirection = "Right"
+		elif Input.is_action_pressed("ui_left"):
+			inputDirection = "Left"
+		elif Input.is_action_pressed("ui_up"):
+			inputDirection = "Up"
+		_animation_player.play("walk" + inputDirection)
+
 func _physics_process(delta: float) -> void:
 	handleInput()
 	move_and_slide()
+	updateAnimation()
